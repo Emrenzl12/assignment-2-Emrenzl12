f24272f6d050d89be815c5555859cf038cb90a12
Emrenzl12 <nazli.emre@ibu.edu.mk>
2026-04-02 00:37:35 +0200

    Update ceasar.c

--- ceasar.c
+++ ceasar.c
@@ -1,5 +1,29 @@
+#include <cs50.h>
 #include <stdio.h>
-int main(void)
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, string argvl[])
 {
-  printf("hello\n");
+  if (argc !=2)
+{
+printf("useage: ./ceasar key \n");
+return 1;
+}
+int key = atoi(argvl[1]);
+string text = get_string("plaintext: ");
+printf("ciphertext: ");
+for (int i = 0; i < strlen(text); i++)
+{
+char c = text[i];
+if(isupper(C))
+{
+printf("%c", (c - 'A' + key) % 26 + 'A');
+}
+else if (islower(C))
+  printf("%c", c)
+    }
+}
+printf("\n");
 }
